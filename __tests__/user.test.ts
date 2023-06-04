5bf5079ebc5652b456ee7d4e5a33eb035c5d19c5
Fran Linde Blázquez <franlindebl@gmail.com>
2023-06-04 23:20:11 +0200

    Added more test

--- __tests__/user.test.ts
+++ __tests__/user.test.ts
@@ -1,8 +1,7 @@
-import { describe } from "node:test";
 import { mongoConnect } from "../src/databases/mongo-db";
 import mongoose from "mongoose";
 import { app, server } from "../src/index";
-import { type IUser } from "../src/models/mongo/User";
+import { User, type IUser } from "../src/models/mongo/User";
 import request from "supertest";
 
 describe("User controller", () => {
@@ -19,8 +18,13 @@ describe("User controller", () => {
     },
   };
 
+  let token: string;
+  let userId: string;
+
   beforeAll(async () => {
     await mongoConnect();
+    await User.collection.drop();
+    console.log("Eliminados todos los usuarios");
   });
 
   afterAll(async () => {
@@ -41,10 +45,101 @@ describe("User controller", () => {
     const response = await request(app)
       .post("/user")
       .send(userMock)
-      .set("Accept", "application/json")
       .expect(201);
 
     expect(response.body).toHaveProperty("_id");
     expect(response.body.email).toBe(userMock.email);
-  })
+
+    userId = response.body._id;
+  });
+
+  it("POST /user/login - with valid credentials returns 200 and token", async () => {
+    const credentials = {
+      email: userMock.email,
+      password: userMock.password
+    };
+
+    const response = await request(app)
+      .post("/user/login")
+      .send(credentials)
+      .expect(200);
+
+    expect(response.body).toHaveProperty("token");
+    token = response.body.token;
+    console.log(token);
+  });
+
+  it("POST /user/login - with worng credentials returns 401 and no token", async () => {
+    const credentials = {
+      email: userMock.email,
+      password: "BAD PASSWORD"
+    };
+
+    const response = await request(app)
+      .post("/user/login")
+      .send(credentials)
+      .expect(401);
+
+    expect(response.body.token).toBeUndefined();
+  });
+
+  it("GET /user - returns a list with the users", async () => {
+    const response = await request(app)
+      .get("/user")
+      .expect(200);
+
+    expect(response.body.data).toBeDefined();
+    expect(response.body.data.length).toBe(1);
+    expect(response.body.data[0].email).toBe(userMock.email);
+    expect(response.body.totalItems).toBe(1);
+    expect(response.body.totalPages).toBe(1);
+    expect(response.body.currentPage).toBe(1);
+  });
+
+  it("PUT /user/id - Modify user when token is sent", async () => {
+    const updatedData = {
+      firstName: "Edu",
+      lastName: "Cuadrado",
+    };
+
+    const response = await request(app)
+      .put(`/user/${userId}`)
+      .set("Authorization", `Bearer ${token}`)
+      .send(updatedData)
+      .expect(200);
+
+    expect(response.body.firstName).toBe(updatedData.firstName);
+    expect(response.body.email).toBe(userMock.email);
+    expect(response.body._id).toBe(userId);
+  });
+
+  it("PUT /user/id - Should not modify user when no token present", async () => {
+    const updatedData = {
+      lastName: "Cuadrado",
+    };
+
+    const response = await request(app)
+      .put(`/user/${userId}`)
+      .send(updatedData)
+      .expect(401);
+
+    expect(response.body.error).toBe("No tienes autorización para realizar esta operación");
+  });
+
+  it("DELETE /user/id -  Do not delete user whe no token is present", async () => {
+    const response = await request(app)
+      .delete(`/user/${userId}`)
+      .expect(401);
+
+    expect(response.body.error).toBe("No tienes autorización para realizar esta operación");
+  });
+
+  it("DELETE /user/id -  Deletes user when token is OK", async () => {
+    const response = await request(app)
+      .delete(`/user/${userId}`)
+      .set("Authorization", `Bearer ${token}`)
+      .expect(200);
+
+    expect(response.body._id).toBe(userId);
+  });
 });
