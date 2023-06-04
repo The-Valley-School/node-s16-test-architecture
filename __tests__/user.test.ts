44c9aaf25d89452e76e134f21089561247234013
Fran Linde Blázquez <franlindebl@gmail.com>
2023-06-04 22:28:08 +0200

    Added test for user POST

--- __tests__/user.test.ts
+++ __tests__/user.test.ts
@@ -1,14 +1,31 @@
 import { describe } from "node:test";
 import { mongoConnect } from "../src/databases/mongo-db";
 import mongoose from "mongoose";
+import { app, server } from "../src/index";
+import { type IUser } from "../src/models/mongo/User";
+import request from "supertest";
 
 describe("User controller", () => {
+  const userMock: IUser = {
+    email: "fran@mail.com",
+    password: "12345678",
+    firstName: "Fran",
+    lastName: "Linde",
+    phone: "666555444",
+    address: {
+      street: "Calle Falsa",
+      number: 123,
+      city: "Madrid",
+    },
+  };
+
   beforeAll(async () => {
     await mongoConnect();
   });
 
   afterAll(async () => {
     await mongoose.connection.close();
+    server.close();
   });
 
   it("Simple test to check jest in working", () => {
@@ -19,4 +36,15 @@ describe("User controller", () => {
     const miTexto = "Hola chicos";
     expect(miTexto.length).toBe(11);
   });
+
+  it("POST /user - this should create an user", async() => {
+    const response = await request(app)
+      .post("/user")
+      .send(userMock)
+      .set("Accept", "application/json")
+      .expect(201);
+
+    expect(response.body).toHaveProperty("_id");
+    expect(response.body.email).toBe(userMock.email);
+  })
 });
